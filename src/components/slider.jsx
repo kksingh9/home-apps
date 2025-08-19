6d239267bf82971c995424f3dea636937c20082e
atlys-gopal <gopal@atlys.com>
2025-08-19 19:14:07 +0530

    sc

--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -26,7 +26,7 @@ export default function HeroSlider() {
     dots: true,
     infinite: true,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 1000,
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
